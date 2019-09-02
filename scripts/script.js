260afe28a50abfa302f64f74d3601ec5e21fc9bb
Lucas Feliciano <lucas.feliciano@bry.com.br>
2019-09-02 17:53:31 -0300

    Adiciona layout mobile

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
 const $header = document.querySelector('header');
 const $logo = document.querySelectorAll('.logo')[0];
 const $navBar = document.querySelectorAll('.nav-bar')[0];
+const $menu = document.querySelectorAll('.menu')[0];
 
 window.addEventListener('scroll', toggleHeader, false);
+$menu.addEventListener('click', toggleMenu, false);
 
 function toggleHeader() {
     if (window.pageYOffset > 60 && $header.classList.contains('max-header')) {
@@ -13,6 +15,8 @@ function toggleHeader() {
         $logo.classList.add('min-logo');
         $navBar.classList.remove('max-nav');
         $navBar.classList.add('min-nav');
+        $menu.firstElementChild.classList.remove('max-hamburger')
+        $menu.firstElementChild.classList.add('min-hamburger')
     } else if (window.pageYOffset <= 60 && $header.classList.contains('min-header')) {
         $header.classList.remove('min-header');
         $header.classList.add('max-header');
@@ -21,5 +25,25 @@ function toggleHeader() {
         $logo.classList.add('max-logo');
         $navBar.classList.remove('min-nav');
         $navBar.classList.add('max-nav');
+        $menu.firstElementChild.classList.add('max-hamburger')
+        $menu.firstElementChild.classList.remove('min-hamburger')
+    }
+}
+
+var isOpen = false;
+function toggleMenu() {
+    if(!isOpen) {
+        $navBar.classList.add('menu-opened');
+        isOpen = true; 
+    } else {
+        $navBar.classList.remove('menu-opened');
+        isOpen = false;
+    }
+}
+
+$navBar.addEventListener('click', navClick, false);
+function navClick(evt) {
+    if (evt.target.tagName == 'A') {
+        toggleMenu();
     }
 }
